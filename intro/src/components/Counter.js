d705a8fa25cc89a4adf16d311d27d0ee21e90bbc
cdufour <opusidea@gmail.com>
2021-08-02 17:10:58 +0200

    j1 end

--- intro/src/components/Counter.js
+++ intro/src/components/Counter.js
@@ -9,40 +9,42 @@ export class Counter extends React.Component {
     message: '' 
   }
 
+  step = 10;
+
   increment() {
-
     this.setState((state) => {
-
-      let count = state.count + 1;
+      let count = state.count + this.step;
 
       if (count > 5) { // condition portant sur le nouvelle état
         return { message: 'Calme-toi !', count }
       } else {
         return { count } 
       }
+    }, () => console.log(this.state.count))
 
-    })
-
+    ;
   }
 
   decrement() {
     if (this.state.count > 0) {
       this.setState({ 
-        count: this.state.count - 1, 
+        count: this.state.count - this.step, 
         message: '' }
       )
     }
   }
   
   render() {
-    
+
+    const style = { marginLeft: this.state.count + 'px' };
+
     return(
       <>
-        <h3>{ this.state.title }</h3>
-        <div>{ this.state.message }</div>
+        <h3 style={style}>{ this.state.title }</h3>
         <span>{ this.state.count }</span>
         <button onClick={ () => this.increment() }>+</button>
         <button onClick={ () => this.decrement() }>-</button>
+        <span>{ this.state.message }</span>
       </>
     )
   }
--- intro/src/components/examples.js
+++ intro/src/components/examples.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export function Simple() {
   return (
     <p>Simple paragraph</p>
@@ -18,16 +20,23 @@ export function Simple2(props) {
 //   )
 // }
 
-function Player({ firstname, lastname, inActivity }) {
+function Player({ firstname, lastname, inActivity, onChangeActivity }) {
+
+  const [active, setActive] = useState(inActivity);
+
   lastname = lastname.toUpperCase();
 
-  var active = inActivity ? 'En activité' : 'Retraité';
+  //var active = inActivity ? 'En activité' : 'Retraité';
+  //if (!inActivity) return (<p>Retour prématuré...</p>);
 
-  if (!inActivity) return (<p>Retour prématuré...</p>);
+  const changeActive = () => {
+    setActive(!active);
+    onChangeActivity(!active);
+  }
 
   return (
     <p>Prénom: { firstname }, Nom: { lastname } 
-      <span onClick={() => { active = '...'; console.log(active) }}>({ active })</span></p>
+      <span onClick={() => { changeActive() }}>({ active ? 'actif' : 'inactif' })</span></p>
   )
 }
 
@@ -42,16 +51,32 @@ function Player({ firstname, lastname, inActivity }) {
 
 export function PlayerList({ dataSource }) {
 
+  let numActive = dataSource
+    .filter((player) => player.inActivity)
+    .length;
+
+  const [numActivePlayers, setNumActivePlayers] = useState(numActive);
+
   const players = dataSource.map(({id, lastname, firstname, inActivity}) => 
     <Player 
       key={id} 
       lastname={lastname} 
       firstname={firstname} 
-      inActivity={inActivity} 
+      inActivity={inActivity}
+      onChangeActivity={(active) => { 
+        if (active) {
+          setNumActivePlayers(numActivePlayers + 1);
+        } else {
+          setNumActivePlayers(numActivePlayers - 1);
+        }
+      }}
     /> );
-  
+
+
   return(
     <>
+      <h3>PlayerList Component</h3>
+      <p>Nombre de joueurs actifs: { numActivePlayers }</p>
       { players }
     </>
   )
